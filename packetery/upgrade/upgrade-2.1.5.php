c6c5ea012cdfc0fa6218810609912f598a34f588
Jan Potužník <jan.potuznik@packeta.com>
2021-03-16 13:57:56 +0100

    PES-64 2.1.5 (v2)

--- packetery/upgrade/upgrade-2.1.5.php
+++ packetery/upgrade/upgrade-2.1.5.php
@@ -9,6 +9,9 @@ function upgrade_module_2_1_5($object)
     return Db::getInstance()->execute('
         DROP TABLE IF EXISTS `' . _DB_PREFIX_ . 'packetery_carrier`;
         
+        DELETE FROM `' . _DB_PREFIX_ . 'packetery_address_delivery`
+        WHERE `id_branch` = 0 AND `name_branch` = "--";
+        
         ALTER TABLE `' . _DB_PREFIX_ . 'packetery_address_delivery`
         CHANGE `id_branch` `id_branch` int(11) NULL,
         CHANGE `name_branch` `name_branch` varchar(255) NULL,
