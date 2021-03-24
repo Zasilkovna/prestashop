51e6afc33a36bd4ec24080945dc95f2ce3addfbd
Jan Potužník <jan.potuznik@packeta.com>
2021-03-24 10:50:15 +0100

    PES-81: Widget v6 support

--- packetery/packetery.php
+++ packetery/packetery.php
@@ -435,13 +435,15 @@ class Packetery extends CarrierModule
 		$name_branch = "";
 		$currency_branch = "";
 		$id_branch = "";
+        $pickupPointType = "";
+        $carrierId = "";
+        $carrierPickupPointId = "";
 		if(!empty($params['cart']))
 		{
 			$row = Db::getInstance()->getRow('SELECT * FROM ' . _DB_PREFIX_ . 'packetery_order WHERE id_cart =' . (int)$params['cart']->id . ' AND id_carrier = ' . (int)$id_carrier);
-			if (!empty($row['id_branch']))
-			{
-				$id_branch = $row['id_branch'];
-			}
+			// to be consistent with widget behavior
+            $id_branch = ($row['is_carrier'] == 1 ? $row['carrier_pickup_point'] : $row['id_branch']);
+
 			if (!empty($row['name_branch']))
 			{
 				$name_branch = $row['name_branch'];
@@ -450,7 +452,10 @@ class Packetery extends CarrierModule
 			{
 				$currency_branch = $row['currency_branch'];
 			}
-		}
+            $pickupPointType = ($row['is_carrier'] == 1 ? 'external' : 'internal');
+            $carrierId = ($row['is_carrier'] == 1 ? $row['id_branch'] : '');
+            $carrierPickupPointId = $row['carrier_pickup_point'];
+        }
 
 		if(isset($params['cart']->id_address_delivery) && !empty($params['cart']->id_address_delivery))
 		{
@@ -465,6 +470,9 @@ class Packetery extends CarrierModule
 		$this->context->smarty->assign('id_branch', $id_branch);
 		$this->context->smarty->assign('name_branch', $name_branch);
 		$this->context->smarty->assign('currency_branch', $currency_branch);
+		$this->context->smarty->assign('pickup_point_type', $pickupPointType);
+		$this->context->smarty->assign('packeta_carrier_id', $carrierId);
+		$this->context->smarty->assign('carrier_pickup_point_id', $carrierPickupPointId);
 
 		$this->context->smarty->assign('ajaxfields', $ajaxfields_json);
 
