993d9d7837e07fe271416bfacd1f937fec7823f9
Jan Potužník <jan.potuznik@gmail.com>
2022-02-01 17:14:41 +0100

    PES-573: rebase onto 2.1.14

--- packetery/packetery.php
+++ packetery/packetery.php
@@ -61,7 +61,7 @@ class Packetery extends CarrierModule
     {
 		$this->name = 'packetery';
 		$this->tab = 'shipping_logistics';
-		$this->version = '2.1.14';
+		$this->version = '2.1.15';
 		$this->author = 'Packeta s.r.o.';
 		$this->need_instance = 0;
     	$this->is_configurable = 1;
--- packetery/upgrade/upgrade-2.1.14.php
+++ packetery/upgrade/upgrade-2.1.14.php
@@ -16,40 +16,4 @@ function upgrade_module_2_1_14(Packetery $module)
         $module->unregisterHook('backOfficeHeader') &&
         $module->registerHook('displayBackOfficeHeader')
     );
-    /* todo 573 widget-hd
-    $result = (
-        Configuration::updateValue('PACKETERY_ADDRESS_VALIDATION', 'none') &&
-        $module->registerHook('actionValidateStepComplete')
-    );
-    if ($result === false) {
-        return false;
-    }
-
-    $db = Db::getInstance();
-    $addressCarriers = Db::getInstance()->executeS(
-        'SELECT `id_carrier` FROM `' . _DB_PREFIX_ . 'packetery_address_delivery` WHERE `pickup_point_type` IS NULL');
-    if ($addressCarriers) {
-        foreach ($addressCarriers as $addressCarrier) {
-            $result = $db->update(
-                'carrier',
-                ['is_module' => 1, 'external_module_name' => 'packetery', 'need_range' => 1],
-                '`id_carrier` = ' . (int)$addressCarrier['id_carrier']);
-            if ($result === false) {
-                return false;
-            }
-        }
-    }
-
-    return $db->execute('
-        ALTER TABLE `' . _DB_PREFIX_ . 'packetery_order`
-        ADD `country` varchar(2) NULL,
-        ADD `county` varchar(255) NULL AFTER `country`,
-        ADD `zip` varchar(255) NULL AFTER `county`,
-        ADD `city` varchar(255) NULL AFTER `zip`,
-        ADD `street` varchar(255) NULL AFTER `city`,
-        ADD `house_number` varchar(255) NULL AFTER `street`,
-        ADD `latitude` varchar(255) NULL AFTER `house_number`,
-        ADD `longitude` varchar(255) NULL AFTER `latitude`;
-    ');
-    */
 }
