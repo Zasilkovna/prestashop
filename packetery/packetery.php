8f526edb651122ba1b006007103c07d298e52a87
Marek Mozola <marek@mozola.net>
2023-03-01 17:19:57 +0100

    PES-1266: add empty lines, remove unused file

--- packetery/packetery.php
+++ packetery/packetery.php
@@ -1472,7 +1472,7 @@ class Packetery extends CarrierModule
 
                         $smarty = new \Smarty();
                         $smarty->assign('trackingNumber', $resultRow[1]);
-                        $packeteryTrackingLink = $smarty->fetch(dirname(__FILE__) . '/views/templates/admin/packeteryTrackingLink.tpl');
+                        $packeteryTrackingLink = $smarty->fetch(dirname(__FILE__) . '/views/templates/admin/trackingLink.tpl');
 
                         $messages[] = [
                             'text' => $this->l('The shipment was successfully submitted under shipment number:') . $packeteryTrackingLink,
