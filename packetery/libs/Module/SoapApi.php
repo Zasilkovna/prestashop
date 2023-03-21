cca0679121df9400acd60d9ff9cdbf7fe05bea1e
Marek Mozola <113614605+MMozola@users.noreply.github.com>
2023-03-21 08:21:10 +0100

    PES-1273: change soap url (#249)
    * PES-1273: change soap url
    
    * PES-1273: constant from Core namespace used
    
    ---------
    
    Co-authored-by: Marek Mozola <marek@mozola.net>
    Co-authored-by: Jan Potužník <jan.potuznik@gmail.com>

--- packetery/libs/Module/SoapApi.php
+++ packetery/libs/Module/SoapApi.php
@@ -3,6 +3,7 @@
 namespace Packetery\Module;
 
 use Packetery;
+use Packetery\Core\Api\Soap\Client;
 use Packetery\Exceptions\SenderGetReturnRoutingException;
 use Packetery\Order\OrderRepository;
 use Packetery\Response\PacketCarrierNumber;
@@ -16,7 +17,7 @@ use SoapFault;
 
 class SoapApi
 {
-    const API_WSDL_URL = 'https://www.zasilkovna.cz/api/soap-php-bugfix.wsdl';
+    const API_WSDL_URL = 'http://www.zasilkovna.cz/api/soap.wsdl';
 
     /**
      * @var Packetery
@@ -44,7 +45,7 @@ class SoapApi
      */
     public function senderGetReturnRouting($senderIndication)
     {
-        $client = new SoapClient(self::API_WSDL_URL);
+        $client = new SoapClient(Client::WSDL_URL);
         try {
             $response = $client->senderGetReturnRouting($this->configHelper->getApiPass(), $senderIndication);
             return $response->routingSegment;
@@ -61,7 +62,7 @@ class SoapApi
     {
         $packetInfo = new PacketInfo();
         try {
-            $client = new SoapClient(self::API_WSDL_URL);
+            $client = new SoapClient(Client::WSDL_URL);
             // get PacketInfoResult
             $response = $client->packetInfo($this->configHelper->getApiPass(), $packetId);
             if (
@@ -121,7 +122,7 @@ class SoapApi
     {
         $response = new PacketCarrierNumber();
         try {
-            $soapClient = new SoapClient(self::API_WSDL_URL);
+            $soapClient = new SoapClient(Client::WSDL_URL);
             $number = $soapClient->packetCourierNumber($this->configHelper->getApiPass(), $packetId);
             $response->setNumber($number);
         } catch (SoapFault $exception) {
