6f31fd6ab107aaaa96743e50a3a7688de5d40680
Jan Potužník <jan.potuznik@gmail.com>
2023-06-07 11:11:46 +0200

    PES-1433: phpdoc, camelCase

--- packetery/libs/Module/Helper.php
+++ packetery/libs/Module/Helper.php
@@ -7,11 +7,12 @@ class Helper
     const TRACKING_URL = 'https://tracking.packeta.com/?id=%s';
 
     /**
+     * @param string $packetId
      * @return string
      */
-    public static function getTrackingUrl($packet_id)
+    public static function getTrackingUrl($packetId)
     {
-        return sprintf(self::TRACKING_URL, rawurlencode($packet_id));
+        return sprintf(self::TRACKING_URL, rawurlencode($packetId));
     }
 
 }
