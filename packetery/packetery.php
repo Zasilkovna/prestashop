59d974bec66036ca9c5a7ec535c91970cbbde4c5
Jan Potužník <jan.potuznik@gmail.com>
2023-07-10 11:01:12 +0200

    PES-1459: product attribute fixes

--- packetery/packetery.php
+++ packetery/packetery.php
@@ -1607,14 +1607,18 @@ class Packetery extends CarrierModule
             return;
         }
 
-        /** @var Packetery\Product\ProductAttributeRepository $productAttribute */
-        $productAttribute = $this->diContainer->get(\Packetery\Product\ProductAttributeRepository::class);
-        $packeteryAgeVerification = $productAttribute->getRow($product->id);
+        $isAdult = null;
+        /** @var Packetery\Product\ProductAttributeRepository $productAttributeRepository */
+        $productAttributeRepository = $this->diContainer->get(\Packetery\Product\ProductAttributeRepository::class);
+        $packeteryAgeVerification = $productAttributeRepository->getRow($product->id);
+        if (is_array($packeteryAgeVerification)) {
+            $isAdult = $packeteryAgeVerification['is_adult'];
+        }
 
         $this->context->smarty->assign([
-                'packeteryAgeVerification' => $packeteryAgeVerification['is_adult'],
-                'adminProductUrl' => $this->getAdminLink('AdminProducts'),
-                'isPrestaShop16' => $isPrestaShop16,
+            'packeteryAgeVerification' => $isAdult,
+            'adminProductUrl' => $this->getAdminLink('AdminProducts'),
+            'isPrestaShop16' => $isPrestaShop16,
         ]);
 
         return $this->display(__FILE__, 'display_admin_product_extra.tpl');
