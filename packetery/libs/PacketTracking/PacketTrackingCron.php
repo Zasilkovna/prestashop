8f9ffc157295b42406ebdfcd0556eb18c8d020a8
Adam Karpita <adam.karpita@packeta.com>
2025-03-03 09:27:27 +0100

    PES-2643: Refactor configuration handling for packet tracking

--- packetery/libs/PacketTracking/PacketTrackingCron.php
+++ packetery/libs/PacketTracking/PacketTrackingCron.php
@@ -68,14 +68,8 @@ class PacketTrackingCron
             ];
         }
 
-        $maxProcessedOrders = ConfigHelper::get('PACKETERY_PACKET_STATUS_TRACKING_MAX_PROCESSED_ORDERS');
-        $maxOrderAgeDays = ConfigHelper::get('PACKETERY_PACKET_STATUS_TRACKING_MAX_ORDER_AGE_DAYS');
         $configOrderStatuses = ConfigHelper::get('PACKETERY_PACKET_STATUS_TRACKING_ORDER_STATES');
-        $configPacketStatuses = ConfigHelper::get('PACKETERY_PACKET_STATUS_TRACKING_PACKET_STATUSES');
-        $isStatusChangeEnabled = ConfigHelper::get('PACKETERY_ORDER_STATUS_CHANGE_ENABLED');
-
         $orderStatuses = Helper::unserialize($configOrderStatuses);
-        $packetStatuses = Helper::unserialize($configPacketStatuses);
 
         if (!is_array($orderStatuses)) {
             return [
@@ -83,13 +77,21 @@ class PacketTrackingCron
                 'class' => 'danger',
             ];
         }
+
+        $configPacketStatuses = ConfigHelper::get('PACKETERY_PACKET_STATUS_TRACKING_PACKET_STATUSES');
+        $packetStatuses = Helper::unserialize($configPacketStatuses);
+
         if (!is_array($packetStatuses)) {
             $packetStatuses = [];
         }
 
+        $maxOrderAgeDays = ConfigHelper::get('PACKETERY_PACKET_STATUS_TRACKING_MAX_ORDER_AGE_DAYS');
         $oldestOrderDate = new DateTimeImmutable("-{$maxOrderAgeDays} days");
+
+        $maxProcessedOrders = ConfigHelper::get('PACKETERY_PACKET_STATUS_TRACKING_MAX_PROCESSED_ORDERS');
         $orders = $this->orderRepository->getOrdersByStateAndLastUpdate($orderStatuses, $maxProcessedOrders, $oldestOrderDate);
 
+        $isStatusChangeEnabled = ConfigHelper::get('PACKETERY_ORDER_STATUS_CHANGE_ENABLED');
         foreach ($orders as $order) {
             $statusRecordsOrErrorMessage = $this->soapApi->getPacketTracking($order['tracking_number']);
 
