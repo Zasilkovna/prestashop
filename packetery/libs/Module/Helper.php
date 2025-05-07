f34c6c3e2ec1b99d73eed4858103d9cc4ff83c3d
Jan Potužník <jan.potuznik@gmail.com>
2025-05-07 09:29:31 +0200

    PES-2782: extracted logic for getting baseUri

--- packetery/libs/Module/Helper.php
+++ packetery/libs/Module/Helper.php
@@ -15,4 +15,12 @@ class Helper
         return sprintf(self::TRACKING_URL, rawurlencode($packetId));
     }
 
+    /**
+     * @return string
+     */
+    public static function getBaseUri()
+    {
+        return __PS_BASE_URI__ === '/' ? '' : Tools::substr(__PS_BASE_URI__, 0, Tools::strlen(__PS_BASE_URI__) - 1);
+    }
+
 }
