ed1dc011287b204dc6f73eb6b852575b090b2a0d
Jan Potužník <jan.potuznik@gmail.com>
2025-06-12 11:26:35 +0200

    PES-2832: version elevated

--- packetery/packetery.php
+++ packetery/packetery.php
@@ -56,7 +56,7 @@ class Packetery extends CarrierModule
     {
         $this->name = 'packetery';
         $this->tab = 'shipping_logistics';
-        $this->version = '3.0.2';
+        $this->version = '3.0.3';
         $this->author = 'Packeta s.r.o.';
         $this->need_instance = 0;
         $this->is_configurable = 1;
