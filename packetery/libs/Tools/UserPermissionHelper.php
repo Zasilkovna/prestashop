d1ae74436444b3a493a94bc471c12901b0421818
Adam Karpita <adam.karpita@packeta.com>
2025-08-15 14:43:31 +0200

    PES-2886: Extract database query logic to UserPermissionRepository

--- packetery/libs/Tools/UserPermissionHelper.php
+++ packetery/libs/Tools/UserPermissionHelper.php
@@ -5,13 +5,14 @@ namespace Packetery\Tools;
 use Context;
 use Exception;
 use Packetery;
+use Packetery\UserPermission\UserPermissionRepository;
 
 class UserPermissionHelper
 {
     public const PERMISSION_VIEW = 'view';
     public const PERMISSION_EDIT = 'edit';
 
-    private const ACCESS_TYPE_MODULE = 'module';
+    public const ACCESS_TYPE_MODULE = 'module';
     private const ACCESS_TYPE_TAB = 'tab';
 
     public const SECTION_ORDERS = 'PacketeryOrderGrid';
@@ -22,14 +23,14 @@ class UserPermissionHelper
     /**
      * @var DbTools
      */
-    private $dbTools;
+    private $userPermissionRepository;
 
     /** @var Packetery */
     private $module;
 
-    public function __construct(DbTools $dbTools, Packetery $module)
+    public function __construct(UserPermissionRepository $userPermissionRepository, Packetery $module)
     {
-        $this->dbTools = $dbTools;
+        $this->userPermissionRepository = $userPermissionRepository;
         $this->module = $module;
     }
 
@@ -50,52 +51,17 @@ class UserPermissionHelper
         }
 
         $modulePermissionRole = 'ROLE_MOD_MODULE_PACKETERY_' . strtoupper($permission === self::PERMISSION_VIEW ? 'READ' : 'UPDATE');
-        $moduleRoleId = $this->findAuthorizationRoleId($modulePermissionRole);
-        if ($moduleRoleId !== null && $this->hasAccessPermission($context->employee->id_profile, $moduleRoleId, self::ACCESS_TYPE_MODULE) === false) {
+        $moduleRoleId = $this->userPermissionRepository->findAuthorizationRoleId($modulePermissionRole);
+        if ($moduleRoleId !== null && $this->userPermissionRepository->hasAccessPermission($context->employee->id_profile, $moduleRoleId, self::ACCESS_TYPE_MODULE) === false) {
             return false;
         }
 
         $tabPermissionRole = 'ROLE_MOD_TAB_' . strtoupper($section) . '_' . strtoupper($permission === self::PERMISSION_VIEW ? 'READ' : 'UPDATE');
-        $tabRoleId = $this->findAuthorizationRoleId($tabPermissionRole);
+        $tabRoleId = $this->userPermissionRepository->findAuthorizationRoleId($tabPermissionRole);
         if ($tabRoleId !== null) {
-            return $this->hasAccessPermission($context->employee->id_profile, $tabRoleId, self::ACCESS_TYPE_TAB);
+            return $this->userPermissionRepository->hasAccessPermission($context->employee->id_profile, $tabRoleId, self::ACCESS_TYPE_TAB);
         }
 
         return false;
-    }
-
-    /**
-     * @param string $slug
-     * @return int|null
-     */
-    private function findAuthorizationRoleId($slug)
-    {
-        try {
-            $result = $this->dbTools->getValue(
-                'SELECT id_authorization_role FROM ' . _DB_PREFIX_ . 'authorization_role WHERE slug = "' . $this->dbTools->db->escape($slug) . '"'
-            );
-            return $result ? (int)$result : null;
-        } catch (Exception $e) {
-            return null;
-        }
-    }
-
-    /**
-     * @param int $profileId
-     * @param int $roleId
-     * @param string $type Use ACCESS_TYPE_MODULE or ACCESS_TYPE_TAB constants
-     * @return bool
-     */
-    private function hasAccessPermission($profileId, $roleId, $type)
-    {
-        try {
-            $table = $type === self::ACCESS_TYPE_MODULE ? 'module_access' : 'access';
-            $result = $this->dbTools->getValue(
-                'SELECT COUNT(*) FROM ' . _DB_PREFIX_ . $table . ' WHERE id_profile = ' . (int)$profileId . ' AND id_authorization_role = ' . (int)$roleId
-            );
-            return $result > 0;
-        } catch (Exception $e) {
-            return false;
-        }
     }
 }
---
+++ packetery/libs/UserPermission/UserPermissionRepository.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Packetery\UserPermission;
+
+use Packetery\Tools\DbTools;
+use Packetery\Tools\UserPermissionHelper;
+
+class UserPermissionRepository
+{
+    private $dbTools;
+
+    public function __construct(DbTools $dbTools)
+    {
+        $this->dbTools = $dbTools;
+    }
+
+    /**
+     * @param string $slug
+     * @return int|null
+     */
+    public function findAuthorizationRoleId($slug)
+    {
+        try {
+            $result = $this->dbTools->getValue(
+                'SELECT id_authorization_role FROM ' . _DB_PREFIX_ . 'authorization_role WHERE slug = "' . $this->dbTools->db->escape($slug) . '"'
+            );
+            return $result ? (int)$result : null;
+        } catch (Exception $e) {
+            return null;
+        }
+    }
+
+    /**
+     * @param int $profileId
+     * @param int $roleId
+     * @param string $type Use ACCESS_TYPE_MODULE or ACCESS_TYPE_TAB constants
+     * @return bool
+     */
+    public function hasAccessPermission($profileId, $roleId, $type)
+    {
+        try {
+            $table = $type === UserPermissionHelper::ACCESS_TYPE_MODULE ? 'module_access' : 'access';
+            $result = $this->dbTools->getValue(
+                'SELECT COUNT(*) FROM ' . _DB_PREFIX_ . $table . ' WHERE id_profile = ' . (int)$profileId . ' AND id_authorization_role = ' . (int)$roleId
+            );
+            return $result > 0;
+        } catch (Exception $e) {
+            return false;
+        }
+    }
+}
