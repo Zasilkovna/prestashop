5341b4010506a2a260921e7e83fa45d3ecb078b4
J7F <J7F@gmail.com>
2026-01-08 11:22:21 +0100

    PES-3038: licence fix

--- PacketeryCsFixerConfig.php
+++ PacketeryCsFixerConfig.php
@@ -13,6 +13,20 @@ class PacketeryCsFixerConfig extends Config
             'after_heredoc' => false,
         ];
 
+        $rules['blank_line_after_opening_tag'] = false;
+
+        $rules['header_comment'] = [
+            'header' => <<<'HEADER'
+@author    Packeta s.r.o. <e-commerce.support@packeta.com>
+@copyright 2017 Packeta s.r.o.
+@license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+HEADER
+,
+            'comment_type' => 'PHPDoc',
+            'location' => 'after_open',
+            'separate' => 'none',
+        ];
+
         return $rules;
     }
 }
--- packetery/upgrade/upgrade-3.1.0.php
+++ packetery/upgrade/upgrade-3.1.0.php
@@ -1,10 +1,9 @@
 <?php
-
 /**
- * (c) Packeta s.r.o. 2017-2026
- * SPDX-License-Identifier: AFL-3.0
+ * @author    Packeta s.r.o. <e-commerce.support@packeta.com>
+ * @copyright 2017 Packeta s.r.o.
+ * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  */
-
 use Packetery\PacketTracking\PacketTrackingRepository;
 use Packetery\Tools\ConfigHelper;
 use Packetery\Tools\DbTools;
